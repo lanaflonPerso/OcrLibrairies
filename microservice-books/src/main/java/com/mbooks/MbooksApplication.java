13feb79ad6783eaafa0938c326a7be344fbd715b
Romain-David <romain-david.sergeant@enedis-grdf.fr>
2019-10-04 08:07:10 +0200

    Création de l'architecture du projet avec les microservices et les edges service

---
+++ microservice-books/src/main/java/com/mbooks/MbooksApplication.java
@@ -0,0 +1,13 @@
+package com.mbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbooksApplication.class, args);
+	}
+
+}
---
+++ microservice-lending/src/main/java/com/lending/MlendingApplication.java
@@ -0,0 +1,13 @@
+package com.lending;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlendingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlendingApplication.class, args);
+	}
+
+}
