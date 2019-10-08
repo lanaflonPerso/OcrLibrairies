308a1161371325b9c3b85cb62e802877d5486e68
Romain-David <romain-david.sergeant@enedis-grdf.fr>
2019-10-08 15:30:55 +0200

    Ajout actuator modification des  des properties

--- microservice-books/src/main/java/com/books/config/ApplicationPropertiesConfig.java
+++ microservice-books/src/main/java/com/books/config/ApplicationPropertiesConfig.java
@@ -3,12 +3,14 @@ package com.books.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties("microservice.config")
 @Getter
 @Setter
+@RefreshScope
 public class ApplicationPropertiesConfig {
 
     private int renewalNumber;
